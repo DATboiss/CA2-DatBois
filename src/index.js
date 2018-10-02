6e4333b8a8f698f785fa8f1f7115ad2c6c8be731
adamSai <adamsaidane@hotmail.com>
2018-10-02 15:10:48 +0200

    Added nmp

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import jokes from "./jokes";
+import cors from "./cors";
+// import cors from "./cors";
+
+// const allJokes = jokes.getJokes().map(joke => "<li>"+joke+"</li>");
+// const oneJoke = jokes.getJokeById(0);
+// document.getElementById("jokes").innerHTML = cors.getPersons();//oneJoke;
+
+
+
