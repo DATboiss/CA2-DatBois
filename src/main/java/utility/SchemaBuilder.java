4c8793352bf51b8289ec65ab3e6ad198de81aeed
EmilVHermansen <32634168+EmilVHermansen@users.noreply.github.com>
2018-10-04 16:03:54 +0200

    Updatede Schemabuilder to have 2 test person objects

--- src/main/java/utility/SchemaBuilder.java
+++ src/main/java/utility/SchemaBuilder.java
@@ -5,6 +5,12 @@
  */
 package utility;
 
+import entity.Address;
+import entity.Cityinfo;
+import entity.Hobby;
+import entity.Person;
+import entity.Phone;
+import facade.PersonFacade;
 import java.util.HashMap;
 import javax.persistence.Persistence;
 
@@ -21,6 +27,27 @@ public class SchemaBuilder
     public static void main(String[] args)
     {
         Persistence.generateSchema("pu", new HashMap<>());
+        PersonFacade pf = new PersonFacade(Persistence.createEntityManagerFactory("pu"));
+        Hobby h1 = new Hobby("Fodbold", "Stor sport");
+        Hobby h2 = new Hobby("Karate", "Kampsport");
+        Cityinfo ci = new Cityinfo("2990", "nivå");
+        Address a = new Address("Hasselhøj", "108", ci);
+        Person p = new Person("Test@gmail.com", "Emil", "Hermansen", a);
+        Phone p1 = new Phone("25582940", "Mobil");
+        p.addHobby(h1);
+        p.addHobby(h2);
+        p.addPhone(p1);
+        pf.addPerson(p);
+        
+        Hobby h3 = new Hobby("Håndbold", "Stor sport");
+        Phone p2 = new Phone("12345678", "Mobil");
+        Cityinfo ci2 = new Cityinfo("2980", "Kokkedal");
+        Address a2 = new Address("Hasselhøj", "108", ci2);
+        Person pp = new Person("Adam@gmail.com", "Adam", "Saidane", a2);
+        pp.addHobby(h3);
+        pp.addPhone(p2);
+        pf.addPerson(pp);
+        
     }
-    
+
 }
