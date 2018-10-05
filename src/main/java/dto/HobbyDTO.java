bfd4bb83d7d2fbbaa727a966425765a34018ad87
Anderskruse <32634165+anderskruse@users.noreply.github.com>
2018-10-05 11:50:45 +0200

    Added HobbyDTO class

---
+++ src/main/java/dto/HobbyDTO.java
@@ -0,0 +1,46 @@
+package dto;
+
+/**
+ *
+ * @author wtfak
+ */
+public class HobbyDTO
+{
+    private String name;
+    private String description;
+
+    public HobbyDTO()
+    {
+
+    }
+
+    public HobbyDTO(String name, String description)
+    {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+    
+    
+    
+
+}
