a5a678cb67335af81ea5d4c5ad86e8049fd40c53
EmilVHermansen <32634168+EmilVHermansen@users.noreply.github.com>
2018-10-05 18:27:38 +0200

    updated SchemaBuilder

--- src/main/java/utility/SchemaBuilder.java
+++ src/main/java/utility/SchemaBuilder.java
@@ -12,7 +12,17 @@ import entity.Person;
 import entity.Phone;
 import facade.PersonFacade;
 import java.util.HashMap;
+import java.util.Map;
+import javax.persistence.Cache;
+import javax.persistence.EntityGraph;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.PersistenceUnitUtil;
+import javax.persistence.Query;
+import javax.persistence.SynchronizationType;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.metamodel.Metamodel;
 
 /**
  *
@@ -27,26 +37,33 @@ public class SchemaBuilder
     public static void main(String[] args)
     {
         Persistence.generateSchema("pu", new HashMap<>());
-        PersonFacade pf = new PersonFacade(Persistence.createEntityManagerFactory("pu"));
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
+        EntityManager em = emf.createEntityManager();
         Hobby h1 = new Hobby("Fodbold", "Stor sport");
         Hobby h2 = new Hobby("Karate", "Kampsport");
-        Cityinfo ci = new Cityinfo("2990", "nivå");
-        Address a = new Address("Hasselhøj 108", "", ci);
-        Person p = new Person("Test@gmail.com", "Emil", "Hermansen", a);
-        Phone p1 = new Phone("25582940", "Mobil");
-        p.addHobby(h1);
-        p.addHobby(h2);
-        p.addPhone(p1);
-        pf.addPerson(p);
+        em.persist(h1);
+        em.persist(h2);
         
-        Hobby h3 = new Hobby("Håndbold", "Stor sport");
-        Phone p2 = new Phone("12345678", "Mobil");
-        Cityinfo ci2 = new Cityinfo("2980", "Kokkedal");
-        Address a2 = new Address("Testvej 1", "3.mf", ci2);
-        Person pp = new Person("Adam@gmail.com", "Adam", "Saidane", a2);
-        pp.addHobby(h3);
-        pp.addPhone(p2);
-        pf.addPerson(pp);
+        
+        
+//        PersonFacade pf = new PersonFacade(Persistence.createEntityManagerFactory("pu"));
+//        Cityinfo ci = new Cityinfo("2990", "nivå");
+//        Address a = new Address("Hasselhøj 108", "", ci);
+//        Person p = new Person("Test@gmail.com", "Emil", "Hermansen", a);
+//        Phone p1 = new Phone("25582940", "Mobil");
+//        p.addHobby(h1);
+//        p.addHobby(h2);
+//        p.addPhone(p1);
+//        pf.addPerson(p);
+//        
+//        Hobby h3 = new Hobby("Håndbold", "Stor sport");
+//        Phone p2 = new Phone("12345678", "Mobil");
+//        Cityinfo ci2 = new Cityinfo("2980", "Kokkedal");
+//        Address a2 = new Address("Testvej 1", "3.mf", ci2);
+//        Person pp = new Person("Adam@gmail.com", "Adam", "Saidane", a2);
+//        pp.addHobby(h3);
+//        pp.addPhone(p2);
+//        pf.addPerson(pp);
         
     }
 
